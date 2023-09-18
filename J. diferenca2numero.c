c58266c78727384b0b77a989ad4a103522cc689e
Madasqui <143505852+Madasqui@users.noreply.github.com>
2023-09-18 07:07:39 -0300

    Update J. diferenca2numero.c

--- J. diferenca2numero.c
+++ J. diferenca2numero.c
@@ -1,11 +1,15 @@
 #include<stdio.h>
+#include<math.h>
 float main()
 {
-    float A, B, C;
+    float A, B, C, D;
     printf("escreva o valor de A:");
     scanf("%f",&A);
     printf("escreva o valor de B:");
     scanf("%f",&B);
     C = A - B;
-    printf("Diferenca de A e B: %.2f ", C); 
+    printf("Diferenca de A e B: %.2f ", C);
+    D = pow(C,2);
+    printf("Quadrado da diferenca de A e B: %.2f ", D);
+    
 }
