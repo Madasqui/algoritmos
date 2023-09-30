41a5aaa18e19342eab46b7eae8055753505e3c11
Madasqui <143505852+Madasqui@users.noreply.github.com>
2023-09-30 12:22:36 -0300

    Add files via upload

---
+++ pag143_34.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+    float NR;
+    scanf("%f",&NR);
+    if(3<NR){
+    printf("numero real maior que 3");
+    }
+     else if(NR<3){
+    printf("numero rel menor que 3");
+    }
+    else {
+    printf("numero real igual a 3");
+    }
+}
