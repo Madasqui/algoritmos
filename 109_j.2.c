f2ab98df7930ffe2807e5b65a07187421abae1ab
Madasqui <143505852+Madasqui@users.noreply.github.com>
2023-10-03 08:11:04 -0300

    Add files via upload

---
+++ 109_j.2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int X;
+    scanf("%i",&X);
+    if(X%2==0)
+    {
+        printf("par");
+    }
+    else
+    {
+        printf("impar");
+    }
+}
+
